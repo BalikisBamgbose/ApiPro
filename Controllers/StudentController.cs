d97529091f87a14ccaed591d07eac405e11ff4a5
BalikisBamgbose <balikisbamgbose@gmail.com>
2023-11-18 10:56:56 +0100

    StartApi

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeApi.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName()
+        {
+            return "Student name";
+        }
+    }
+}
